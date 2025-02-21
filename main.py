ffc85a8775c16cb34012858d401c3b6c117fa10d
Ochir23 <ochir230413@gmail.com>
2025-02-21 20:32:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное"
-            "МЕМ":"Прикол"
-            }
+import telebot
+    
+# Инициализация бота с использованием его токена
+bot = telebot.TeleBot("tOkEn")
 
-word = input("Введите непонятное слово (большими буквами!): ")
+# Обработчик команды '/start' и '/hello'
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
 
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print('В словаре нет такого слова')
+# Обработчик команды '/heh'
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+
+
+@bot.message_handler(commands=['rickroll'])
+def send_welcome(message):
+    bot.reply_to(message, 'https://youtu.be/Cxmvq1MCR3c?si=Ekv_xd7X6YW_6ulJ')
+
+bot.message_handler(commands=['how are you?'])
+def send_welcome(message):
+    bot.reply_to(message, 'i am fine,thanks.')
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+
+
+# Запуск бота
+bot.polling()
