f7d80cb6d50e2f231cac381b1ca3bf9364d5e3c8
Ochir23 <ochir230413@gmail.com>
2025-03-03 12:35:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import telebot
-    
+import random 
+import os
+
+
 # Инициализация бота с использованием его токена
 bot = telebot.TeleBot("token")
 
 # Обработчик команды '/start' и '/hello'
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!Вот доступные команды:/you /do /color /mood /health')
+    bot.reply_to(message, f'Привет! Я просто бот! Вот доступные команды:/mem, /rickrol, /animals, /how are you, /heh')
 
 # Обработчик команды '/heh'
 @bot.message_handler(commands=['heh'])
@@ -16,29 +19,29 @@ def send_heh(message):
 
 
 @bot.message_handler(commands=['rickroll'])
-def send_rickrol(message):
+def send_welcome(message):
     bot.reply_to(message, 'https://youtu.be/Cxmvq1MCR3c?si=Ekv_xd7X6YW_6ulJ')
 
-bot.message_handler(commands=['you'])
-def send_you(message):
-    bot.reply_to(message, f'Отлично')
+bot.message_handler(commands=['how are you?'])
+def send_welcome(message):
+    bot.reply_to(message, 'i am fine,thanks.')
 
-bot.message_handler(commands=['do'])
-def send_do(message):
-    bot.reply_to(message, f'Ищу ошибку в твоем коде.')
+@bot.message_handler(commands=['mem'])
+def send_mem(message):
+    file_list = os.listdir("images")
+    img_name = random.choice(file_list)
+    with open(f'images/{img_name}', 'rb') as f:
+        bot.send_photo(message.chat.id, f)
 
-bot.message_handler(commands=['color'])
-def send_color(message):
-    bot.reply_to(message, f'#800080.')
+@bot.message_handler(commands=['animals'])
+def send_animals(message):
+    file_list = os.listdir("animals")
+    img_name = random.choice(file_list)
+    with open(f'animals/{img_name}', 'rb') as f:
+        bot.send_photo(message.chat.id, f)
 
-bot.message_handler(commands=['health'])
-def send_health(message):
-    bot.reply_to(message, f'Если не будешь качать торренты, то не буду болеть.')
 
-bot.message_handler(commands=['mood'])
-def send_mood(message):
-    bot.reply_to(message, f'Пока хорошее.')
-
+ 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
